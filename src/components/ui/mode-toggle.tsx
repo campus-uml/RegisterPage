14d92de0e7a2a71db86e4af20ab47893a0305414
jeseeliam <est.cbetancurb0301@uml.edu.ni>
2025-03-06 19:30:01 -0600

    fix(mode-toggle): Corregir accion al hace el click en el boton de modo oscuro . -Se corrigio la accion al hacer click en el boton de modo oscuro, ahora se activa y desactiva correctamente.

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -1,8 +1,14 @@
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "../../components/ui/theme-provider";
+import { useEffect } from "react";
 
 export function ModeToggle() {
   const { theme: currentTheme, setTheme } = useTheme();
+  useEffect(() => {
+    if (!currentTheme) {
+      setTheme("light");
+    }
+  }, [currentTheme, setTheme]);
 
   return (
     <button
@@ -11,9 +17,9 @@ export function ModeToggle() {
       onClick={() => setTheme(currentTheme === "light" ? "dark" : "light")}
     >
       {currentTheme === "light" ? (
-      <Moon data-testid="moon-icon" className="w-6 h-6" />
+        <Moon data-testid="moon-icon" className="w-6 h-6" />
       ) : (
-      <Sun data-testid="sun-icon" className="w-6 h-6" />
+        <Sun data-testid="sun-icon" className="w-6 h-6" />
       )}
     </button>
   );
